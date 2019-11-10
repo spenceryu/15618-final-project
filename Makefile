0105121ddf6470f92162da3d4eff8c9bbb549feb
Ramachandra Mannava <rmannava@andrew.cmu.edu>
2019-11-10 17:24:26 -0500

    move test code to main

--- Makefile
+++ Makefile
@@ -14,6 +14,7 @@ LOGS	   := logs
 
 OBJDIR=objs
 IMGDIR=images
+PNGDIR=lodepng
 CXX=g++ -m64
 CXXFLAGS=-O3 -Wall
 LDFLAGS=-L/usr/local/depot/cuda-8.0/lib64/ -lcudart
@@ -21,27 +22,28 @@ NVCC=nvcc
 NVCCFLAGS=-O3 -m64 --gpu-architecture compute_35
 
 
-OBJS=$(OBJDIR)/main.o  $(OBJDIR)/jpeg.o $(OBJDIR)/jpeg-seq.o
+OBJS=$(OBJDIR)/main.o  $(OBJDIR)/jpeg.o $(OBJDIR)/jpeg-seq.o $(OBJDIR)/$(PNGDIR)/lodepng.o
 
 
-.PHONY: dirs clean lodepng_test
+.PHONY: default dirs clean lodepng
 
 default: $(EXECUTABLE)
 
 dirs:
 		mkdir -p $(OBJDIR)
+		mkdir -p $(OBJDIR)/$(PNGDIR)
 		mkdir -p $(IMGDIR)
 
 clean:
 		rm -rf $(OBJDIR) $(IMGDIR) *~ $(EXECUTABLE) $(LOGS)
 
-lodepng_test:
-		g++ src/lodepng/lodepng.cpp src/load_png.cpp -ansi -pedantic -Wall -Wextra -O3
-
 $(EXECUTABLE): dirs $(OBJS)
 		$(CXX) $(CXXFLAGS) -o $@ $(OBJS) $(LDFLAGS)
 
 $(OBJDIR)/%.o: src/%.cpp
+		$(CXX) $< $(CXXFLAGS) -c -o $@
+
+$(OBJDIR)/$(PNGDIR)/%.o: src/$(PNGDIR)/%.cpp
 		$(CXX) $< $(CXXFLAGS) -c -o $@
 
 $(OBJDIR)/%.o: src/%.cu
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,35 @@
+#include <iostream>
+#include "stdio.h"
+#include "lodepng/lodepng.h"
+
+void decodeOneStep(const char* filename) {
+  std::vector<unsigned char> image; //the raw pixels
+  unsigned width, height;
+
+  //decode
+  unsigned error = lodepng::decode(image, width, height, filename);
+
+  //if there's an error, display it
+  if(error) {
+      std::cout << "decoder error " << error << ": " << lodepng_error_text(error) << std::endl;
+  } else {
+      fprintf(stdout, "success decoding %s!\n", filename);
+  }
+
+  //the pixels are now in the vector "image", 4 bytes per pixel, ordered RGBARGBA..., use it as texture, draw it, ...
+
+  const char* outfile = "images/out.png";
+  error = lodepng::encode(outfile, image, width, height);
+
+  //if there's an error, display it
+  if(error) {
+    std::cout << "encoder error " << error << ": "<< lodepng_error_text(error) << std::endl;
+  } else {
+    fprintf(stdout, "success encoding to %s!\n", outfile);
+  }
+}
+
 int main() {
-    return 1;
+    decodeOneStep("raw_images/cookie.png");
+    return 0;
 }
