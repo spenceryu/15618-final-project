0877ca14e213a3b97088a16b07528b006dd4d9f4
Ramachandra Mannava <rmannava@andrew.cmu.edu>
2019-11-10 17:52:03 -0500

    add image interface

--- src/image.h
+++ src/image.h
@@ -6,21 +6,22 @@ struct pixel_rgba {
 }
 
 struct pixel_ycbcr {
-    unsigned char y;
-    unsigned char cb;
-    unsigned char cr;
+    double y;
+    double cb;
+    double cr;
 }
 
 typedef struct ImageRGB {
-    std::vector<pixel_rgba> image;
+    std::vector<pixel_rgba> pixels;
     int width;
     int height;
 } image_rgb;
 
 typedef struct ImageYCbCr {
-    std::vector<pixel_ycbcr> image;
+    std::vector<pixel_ycbcr> pixels;
     int width;
     int height;
 } image_ycbcr;
 
-
+image_ycbcr convert_rgb_ycbcr(image_rgb input);
+image_rgb convert_ycbcr_rgb(image_ycbcr input);
