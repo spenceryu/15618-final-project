6c157a644847634122bd77a89e4a704dd1b6250f
swyu <swyu@andrew.cmu.edu>
2019-11-10 17:12:39 -0500

    update load_png to do output back to png as module test

--- src/load_png.cpp
+++ src/load_png.cpp
@@ -13,10 +13,20 @@ void decodeOneStep(const char* filename) {
   if(error) {
       std::cout << "decoder error " << error << ": " << lodepng_error_text(error) << std::endl;
   } else {
-      fprintf(stdout, "success!\n");
+      fprintf(stdout, "success decoding %s!\n", filename);
   }
 
   //the pixels are now in the vector "image", 4 bytes per pixel, ordered RGBARGBA..., use it as texture, draw it, ...
+
+  const char* outfile = "out.png";
+  error = lodepng::encode(outfile, image, width, height);
+
+  //if there's an error, display it
+  if(error) {
+    std::cout << "encoder error " << error << ": "<< lodepng_error_text(error) << std::endl;
+  } else {
+    fprintf(stdout, "success encoding to %s!\n", outfile);
+  }
 }
 
 int main() {
