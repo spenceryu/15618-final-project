aeb801b4dca364fd46e775f0eaadfa7a6b5e44ed
swyu <swyu@andrew.cmu.edu>
2019-11-24 21:40:59 -0500

    clean up main

--- src/main.cpp
+++ src/main.cpp
@@ -9,42 +9,6 @@
 
 #define MACROBLOCK_SIZE 8
 
-void testFnInv(const char* infile, const char* outfile) {
-    std::vector<unsigned char> bytes; //the raw pixels
-    unsigned int width, height;
-
-    //decode
-    unsigned int error = lodepng::decode(bytes, width, height, infile);
-
-    //if there's an error, display it
-    if(error) {
-      std::cout << "decoder error " << error << ": " << lodepng_error_text(error) << std::endl;
-    } else {
-      fprintf(stdout, "success decoding %s!\n", infile);
-    }
-    
-    fprintf(stdout, "starting encryption\n");
-
-    std::shared_ptr<ImageRgb> imageRgb = convertBytesToImage(bytes, width, height);
-    std::shared_ptr<ImageYcbcr> imageYcbcr = convertRgbToYcbcr(imageRgb);
-    std::shared_ptr<ImageBlocks> imageBlocks = convertYcbcrToBlocks(imageYcbcr, MACROBLOCK_SIZE);
-
-    fprintf(stdout, "starting decryption\n");
-
-    std::shared_ptr<ImageYcbcr> imgFromBlocks = convertBlocksToYcbcr(imageBlocks, MACROBLOCK_SIZE);
-    std::shared_ptr<ImageRgb> imageRgbRecovered = convertYcbcrToRgb(imgFromBlocks);
-    bytes = convertImageToBytes(imageRgbRecovered);
-
-    error = lodepng::encode(outfile, bytes, width, height);
-
-    //if there's an error, display it
-    if(error) {
-        std::cout << "encoder error " << error << ": "<< lodepng_error_text(error) << std::endl;
-    } else {
-        fprintf(stdout, "success encoding to %s!\n", outfile);
-    }
-}
-
 void decodeOneStep(const char* infile, const char* outfile, const char* compressedFile) {
     std::vector<unsigned char> bytes; //the raw pixels
     unsigned int width, height;
@@ -145,8 +109,6 @@ void decodeOneStep(const char* infile, const char* outfile, const char* compress
 }
 
 int main() {
-    /* decodeOneStep("raw_images/cookie.png", "images/cookie.png", "compressed/cookie.jpeg"); */
-    testFnInv("raw_images/cookie.png", "images/cookie.png");
-    /* testFnInv("raw_images/cookie_small.png", "images/cookie_small.png"); */
+    decodeOneStep("raw_images/cookie.png", "images/cookie.png", "compressed/cookie.jpeg");
     return 0;
 }
