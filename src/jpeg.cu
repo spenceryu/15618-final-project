7d2fe2ba631e6f5aeee3578f0408959f1d95dd37
swyu <swyu@andrew.cmu.edu>
2019-12-05 13:41:44 -0500

    cu

---
+++ src/jpeg.cu
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include <driver_functions.h>
+
+#define DEBUG
+
+#ifdef DEBUG
+#define cudaCheckError(ans) cudaAssert((ans), __FILE__, __LINE__);
+
+inline void cudaAssert(cudaError_t code, const char *file, int line,
+                       bool abort=true) {
+    if(code != cudaSuccess) {
+        fprintf(stderr, "CUDA Error: %s at %s:%d\n", cudaGetErrorString(code), file,
+                line);
+        if(abort) {
+            exit(code);
+        }
+    }
+}
+
+#else
+#define cudaCheckError(ans) ans
+#endif
+
